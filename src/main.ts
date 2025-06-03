c1f1e660a7e7ee7f9f4ba50f0537f02d61417c18
paul_narvaez <jpnarvaez@sudamericano.edu.ec>
2025-06-03 18:44:59 -0500

    backend listo1

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
- const app = await NestFactory.create(AppModule); // Configura CORS para permitir tu frontend desdeVercel
- app.enableCors({
- origin: process.env.FRONTEND_URL || '*', //Permite tu dominio o todos (solo para pruebas)
- });
+  const app = await NestFactory.create(AppModule);
 
- const port = process.env.PORT || 3000;
- await app.listen(port);
+  app.enableCors({
+    origin: ['https://fronted-semanaa8-wd5e.vercel.app'], // permite solo tu frontend
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
