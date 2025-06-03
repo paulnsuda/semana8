39fb7091b2d0f224da3192ffafbf795039994cf1
paul_narvaez <jpnarvaez@sudamericano.edu.ec>
2025-06-03 18:28:32 -0500

    backend listo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotificationsModule } from './notifications/notifications.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-imports: [
-  TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '0980993908P',
-      database: 'semana8',
-      autoLoadEntities: true,
-      synchronize: true, 
-    }),
-  NotificationsModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+ imports: [
+ ConfigModule.forRoot({
+ isGlobal: true,
+ }),
+ TypeOrmModule.forRoot({
+ type: 'postgres',
+ url: process.env.DATABASE_URL,
+ autoLoadEntities: true,
+ synchronize: true,
+ }),
+ NotificationsModule,
+ ],
+ controllers: [AppController],
+ providers: [AppService],
 })
+
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+ const app = await NestFactory.create(AppModule); // Configura CORS para permitir tu frontend desdeVercel
+ app.enableCors({
+ origin: process.env.FRONTEND_URL || '*', //Permite tu dominio o todos (solo para pruebas)
+ });
+
+ const port = process.env.PORT || 3000;
+ await app.listen(port);
 }
+
 bootstrap();
